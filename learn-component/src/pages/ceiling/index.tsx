d6acac4bb0599d055dc29b89fe64336141a416d6
张康 <zhangkang@zhangkangs-MacBook-Pro.local>
2021-08-04 16:12:16 +0800

    下拉加载+吸顶

--- learn-component/src/pages/ceiling/index.tsx
+++ learn-component/src/pages/ceiling/index.tsx
@@ -1,21 +1,67 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './index.less';
 import ClassNames from 'classnames';
+import BScroll from '@better-scroll/core'
+import Pullup from '@better-scroll/pull-up'
 
-const mockData = new Array(80).fill(1);
+BScroll.use(Pullup);
+
 
 function Index() {
+  const BScrollRef = useRef<any>();
 
   const [show, setShow] = useState(true);
+  const [isPullUpLoad, setIsPullUpLoad] = useState(false);
+  const [mockData, setMockData] = useState(new Array(20).fill(1))
 
+  async function pullingUpHandler() {
+    setIsPullUpLoad(true)
+    await requestData()
+
+    BScrollRef.current.finishPullUp()
+    BScrollRef.current.refresh()
+    setIsPullUpLoad(false)
+  }
+  async function requestData() {
+    try {
+      const newData = await ajaxGet(/* url */)
+      // 使用函数数setState可以获取最新的state值
+      setMockData((mock) => [...mock, ...new Array(newData).fill(1)]);
+    } catch (err) {
+      // handle err
+      console.log(err)
+    }
+  }
+
+  function ajaxGet(/* url */) {
+    return new Promise(resolve => {
+      setTimeout(() => {
+        resolve(20)
+      }, 1000)
+    })
+  }
+
+  useEffect(() => {
+    BScrollRef.current = new BScroll('.scroll-container', {
+      pullUpLoad: true,
+      specifiedIndexAsContent: 1,
+      // probeType: 3
+    })
+
+    BScrollRef.current.on('pullingUp', pullingUpHandler)
+    return () => {
+      BScrollRef.current.destroy();
+    }
+  }, [])
+  
   useEffect(() => {
     let nav = document.querySelector('nav');
     let reference = document.querySelector(".scroll-item") as Element;
-    console.log(reference, '--reference--');
+    // console.log(reference, '--reference--');
     const myObserver = new IntersectionObserver(entries => {
-      console.log(entries, '--entries--');
+      // console.log(entries, '--entries--');
       let item = entries[0];
-      console.log(item, '--item--');
+      // console.log(item, '--item--');
       if(item.boundingClientRect.top < 40){
         console.log('可以吸顶了')
         setShow(false);
@@ -29,11 +75,12 @@ function Index() {
       rootMargin: '-72px 0px', // 60px，目的是声明顶部有个60px的距离需要减掉
     })
     myObserver.observe(reference);
+
     return () => {
       myObserver.disconnect();
     }
   }, [])
-  
+
   const headerCls = ClassNames('ceiling--header', {
     visible: show
   })
@@ -41,7 +88,6 @@ function Index() {
   const stickyCls = ClassNames('sticky-box', {
     top: !show
   })
-
   return (
     <div className="ceiling--wrapper">
       <div className={headerCls}>
@@ -62,6 +108,14 @@ function Index() {
               )
             })
           }
+          <div className="pullup-tips">
+            {
+              isPullUpLoad && 'Loading...'
+            }
+            {
+              !isPullUpLoad && '加载更多'
+            }
+          </div>
         </div>
       </div>
     </div>
